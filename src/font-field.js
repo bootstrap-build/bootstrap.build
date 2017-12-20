ac5747a5290150f178d1857c585f147ba1c8f12e
Oleg Berman <bermanoleg@gmail.com>
2017-12-19 20:54:54 -0500

    google fonts auto suggest

--- src/font-field.js
+++ src/font-field.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react'
-import { Input } from 'antd'
+import { AutoComplete } from 'antd'
+import fonts from './google-fonts.json'
+
+const fontNames = fonts.map(font => font.family)
 
 class FontField extends Component {
-  
-  handleChange = event => {
-    this.props.onChange(event.target.value)
+
+  handleChange = value => {
+    this.props.onChange(value)
   }
 
   render() {
     return (
-      <Input value={this.props.value} onChange={this.handleChange} />
+      <AutoComplete
+        dataSource={fontNames}
+        style={{ width: '100%' }}
+        placeholder="Font Family"
+        value={this.props.value}
+        onChange={this.handleChange}
+        filterOption={(inputValue, option) => option.props.children.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1}
+      />
     )
   }
 }
