02c03faa6b7fb2d1554d9f64023b516a1cf31c3b
Oleg Berman <bermanoleg@gmail.com>
2018-02-11 18:54:51 -0500

    basic versioning

---
+++ v4.0.0-beta.2/src/elements.js
@@ -0,0 +1,108 @@
+const items = [
+  {
+    text: 'Core',
+    template: './templates/core.html'
+  },
+  {
+    text: 'Colors',
+    template: './templates/colors.html'
+  },
+  {
+    text: 'Grid',
+    template: './templates/grid.html'
+  },
+  {
+    text: 'Typography',
+    template: './templates/typography.html'
+  },
+  {
+    text: 'Tables',
+    template: './templates/tables.html'
+  },
+  {
+    text: 'Buttons',
+    template: './templates/buttons.html'
+  },
+  {
+    text: 'Forms',
+    template: './templates/forms.html'
+  },
+  {
+    text: 'Dropdowns',
+    template: './templates/dropdowns.html'
+  },
+  {
+    text: 'Navs',
+    template: './templates/navs.html'
+  },
+  {
+    text: 'Navbar',
+    template: './templates/navbar.html'
+  },
+  {
+    text: 'Pagination',
+    template: './templates/pagination.html'
+  },
+  {
+    text: 'Jumbotron',
+    template: './templates/jumbotron.html'
+  },
+  {
+    text: 'Cards',
+    template: './templates/card.html'
+  },
+  {
+    text: 'Tooltips',
+    template: './templates/tooltips.html'
+  },
+  {
+    text: 'Popovers',
+    template: './templates/popovers.html'
+  },
+  {
+    text: 'Badge',
+    template: './templates/badge.html'
+  },
+  {
+    text: 'Modals',
+    template: './templates/modal.html'
+  },
+  {
+    text: 'Alerts',
+    template: './templates/alerts.html'
+  },
+  {
+    text: 'Progress',
+    template: './templates/progress.html'
+  },
+  {
+    text: 'List group',
+    template: './templates/list-group.html'
+  },
+  {
+    text: 'Images',
+    template: './templates/images.html'
+  },
+  {
+    text: 'Figures',
+    template: './templates/figures.html'
+  },
+  {
+    text: 'Breadcrumb',
+    template: './templates/breadcrumb.html'
+  },
+  {
+    text: 'Carousel',
+    template: './templates/carousel.html'
+  },
+  {
+    text: 'Close',
+    template: './templates/close.html'
+  },
+  {
+    text: 'Code',
+    template: './templates/code.html'
+  }
+]
+
+export default items
