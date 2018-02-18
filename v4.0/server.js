3d4cfdbbeed16cc3778d6053dd49d603c6c0b8aa
Oleg Berman <bermanoleg@gmail.com>
2018-02-18 17:04:46 -0500

    fixes

--- v4.0/server.js
+++ v4.0/server.js
@@ -5,11 +5,6 @@ const app = express()
 
 app.use(express.static('build'))
 
-app.get('*', (req, res, next) => {
-  const fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
-  console.log(fullUrl)
-})
-
 app.listen(3001, () => {
   console.log('👍 Production Front End server is up')
 })
