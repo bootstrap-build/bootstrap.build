72645692fefd685dc226063670304302ee5c372e
Oleg Berman <bermanoleg@gmail.com>
2018-02-18 17:00:56 -0500

    logging requests to debug

--- v4.0/server.js
+++ v4.0/server.js
@@ -5,6 +5,11 @@ const app = express()
 
 app.use(express.static('build'))
 
+app.get('*', (req, res, next) => {
+  const fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+  console.log(fullUrl)
+})
+
 app.listen(3001, () => {
   console.log('👍 Production Front End server is up')
 })
