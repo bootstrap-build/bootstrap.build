5fe222e963ea2300140c59bc32fcab1c1e1da558
Oleg Berman <bermanoleg@gmail.com>
2018-02-22 09:31:14 -0500

    removed google ads

--- v4.0/src/app.js
+++ v4.0/src/app.js
@@ -417,16 +417,6 @@ class App extends Component {
           <div className="preview__content" style={{ display: this.state.loading ? 'none' : 'block' }}>
             {iframe}
           </div>
-          <div className="ads">
-            <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-            <ins className="adsbygoogle"
-                 style={{ display:'inline-block', width:728, height:90 }}
-                 data-ad-client="ca-pub-7062432953325416"
-                 data-ad-slot="7777761719"></ins>
-            <script>
-            (adsbygoogle = window.adsbygoogle || []).push({});
-            </script>
-          </div>
         </div>
       </div>
     );
