ce86e708ea5383598d8d89d5f672545e7a1eb4a5
Oleg Berman <bermanoleg@gmail.com>
2018-02-21 21:22:52 -0500

    moved tracking code

--- v4.0/src/app.js
+++ v4.0/src/app.js
@@ -427,16 +427,6 @@ class App extends Component {
             (adsbygoogle = window.adsbygoogle || []).push({});
             </script>
           </div>
-          <script>{`
-              (function(h,o,t,j,a,r){
-                  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                  h._hjSettings={hjid:789419,hjsv:6};
-                  a=o.getElementsByTagName('head')[0];
-                  r=o.createElement('script');r.async=1;
-                  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                  a.appendChild(r);
-              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-          `}</script>
         </div>
       </div>
     );
